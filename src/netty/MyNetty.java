dff1bf22725322382e1b637521665ede3f562976
RhysNi <java_nsd@163.com>
2022-08-07 20:50:23 +0800

    Netty原生API写法

--- src/netty/MyNetty.java
+++ src/netty/MyNetty.java
@@ -1,8 +1,12 @@
 package netty;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.*;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.ServerSocketChannel;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import org.junit.Test;
@@ -103,10 +107,10 @@ public class MyNetty {
     @Test
     public void loopExecutor() throws IOException {
         //NioEventLoopGroup本身并非多路复用器而是相当于线程池，多路复用器概念是属于由NioEventLoopGroup new出来的线程
-        NioEventLoopGroup selector = new NioEventLoopGroup(2);
+        NioEventLoopGroup group = new NioEventLoopGroup(2);
 
         //selector.execute执行数量取决于NioEventLoopGroup中可创建的线程数
-        selector.execute(() -> {
+        group.execute(() -> {
             try {
                 for (; ; ) {
                     out.println("selector--1");
@@ -117,7 +121,7 @@ public class MyNetty {
             }
         });
 
-        selector.execute(() -> {
+        group.execute(() -> {
             try {
                 for (; ; ) {
                     out.println("selector--2");
@@ -141,11 +145,10 @@ public class MyNetty {
      */
     @Test
     public void clientModel() throws InterruptedException {
-        ChannelFuture future;
-        NioEventLoopGroup thread = new NioEventLoopGroup(1);
+        NioEventLoopGroup group = new NioEventLoopGroup(1);
         NioSocketChannel client = new NioSocketChannel();
         //将客户端注册到NioEventLoopGroup
-        thread.register(client);
+        group.register(client);
 
         //客户端有事件到达多路复用器 基础事件通过管道中调用后续操作的handler
         ChannelPipeline pipeline = client.pipeline();
@@ -154,34 +157,108 @@ public class MyNetty {
 
         //客户端连接服务端
         //由于reactor操作都是异步的，所以要利用Future来接收响应数据
-        future = client.connect(new InetSocketAddress("192.168.2.237", 9090));
+        ChannelFuture connect = client.connect(new InetSocketAddress("192.168.2.237", 9090));
         //等待connectFuture
-        future.sync();
+        ChannelFuture connectFuture = connect.sync();
 
         //拷贝字节数组
         ByteBuf buf = Unpooled.copiedBuffer("Hello Netty".getBytes(StandardCharsets.UTF_8));
         //客户端刷写数据
-        future = client.writeAndFlush(buf);
-        //阻塞等待数据成功发送并阻塞等待Client断开连接
-        future.sync().channel().closeFuture().sync();
+        ChannelFuture send = client.writeAndFlush(buf);
+        //阻塞等待数据成功发送
+        send.sync();
+
+        //阻塞等待Client断开连接
+        connectFuture.channel().closeFuture().sync();
         out.println("Client断开连接~~~");
     }
 
+    /**
+     * 服务端模式
+     *
+     * @param
+     * @return void
+     * @author Rhys.Ni
+     * @date 2022/8/7
+     */
     @Test
     public void serverMode() throws InterruptedException {
-        ChannelFuture future;
-        NioEventLoopGroup thread = new NioEventLoopGroup(1);
+        NioEventLoopGroup group = new NioEventLoopGroup(1);
         NioServerSocketChannel server = new NioServerSocketChannel();
 
-        thread.register(server);
+        group.register(server);
 
         ChannelPipeline pipeline = server.pipeline();
         //Accept由此处理
-        pipeline.addLast(new AcceptHandler(thread, new ChannelInit()));
+        pipeline.addLast(new AcceptHandler(group, new ChannelInit()));
 
         //对于服务端Accept后 -> Bind
-        future = server.bind(new InetSocketAddress("192.168.2.103", 9090));
+        ChannelFuture bind = server.bind(new InetSocketAddress("192.168.2.103", 9090));
         //阻塞等待连接成功并阻塞等待Server断开连接
-        future.sync().channel().closeFuture().sync();
+        bind.sync().channel().closeFuture().sync();
+    }
+
+    /**
+     * netty官方版客户端写法
+     *
+     * @param
+     * @return void
+     * @author Rhys.Ni
+     * @date 2022/8/7
+     */
+    @Test
+    public void nettyClient() throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup(1);
+        Bootstrap bootstrap = new Bootstrap();
+        ChannelFuture connect = bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        ChannelPipeline pipeline = socketChannel.pipeline();
+                        pipeline.addLast(new InHandler());
+                    }
+                })
+                .connect(new InetSocketAddress("192.168.2.103", 9090));
+
+        //阻塞同步接收客户端channel
+        Channel client = connect.sync().channel();
+
+        //拷贝字节数组
+        ByteBuf buffer = Unpooled.copiedBuffer("".getBytes(StandardCharsets.UTF_8));
+        //客户端刷写数据
+        ChannelFuture send = client.writeAndFlush(buffer);
+        //阻塞等待数据成功发送并阻塞等待Client断开连接
+        send.sync();
+
+        //阻塞等待Client断开连接
+        client.closeFuture().sync();
+        out.println("Client断开连接~~~");
+    }
+
+    /**
+     * netty官方版服务端写法
+     *
+     * @param
+     * @return void
+     * @author Rhys.Ni
+     * @date 2022/8/7
+     */
+    @Test
+    public void nettyServer() throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup(1);
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        //需要传两个group 额外一个用于处理Accept
+        ChannelFuture bind = serverBootstrap.group(group, group)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        ChannelPipeline pipeline = socketChannel.pipeline();
+                        pipeline.addLast(new InHandler());
+                    }
+                })
+                .bind(new InetSocketAddress("192.168.2.103", 9090));
+        bind.sync().channel().closeFuture().sync();
     }
 }
