de3e9d5c165417e8a3dbce2bf9dfcfe89a531f4f
RhysNi <java_nsd@163.com>
2022-08-07 20:54:15 +0800

    Netty原生API写法

--- src/netty/MyNetty.java
+++ src/netty/MyNetty.java
@@ -5,7 +5,6 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.*;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.ServerSocketChannel;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
